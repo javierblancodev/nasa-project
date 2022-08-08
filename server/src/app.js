eff78de6005782bc0f23dcf033ada506ec22b039
Javier Blanco <javierblancoit@gmail.com>
2022-08-08 08:32:53 +0100

    set cors middleware to allow request coming from the frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // Parse any incoming json from the body of any incoming request
 app.use(express.json())
