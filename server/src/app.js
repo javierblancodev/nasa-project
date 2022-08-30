09208317b98c5f201a8ba36f4715c3aca20cfcd8
Javier Blanco <javierblancoit@gmail.com>
2022-08-30 08:13:18 +0100

    get project ready to be served in a production fashion

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -11,6 +12,12 @@ app.use(cors({
 
 // Parse any incoming json from the body of any incoming request
 app.use(express.json())
+
+// Serve all our public files
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 // Add the middleware planetsRouter on the top of our express app (chain of middleware)
 app.use(planetsRouter);
